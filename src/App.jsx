7a1b35f6c6beeaade74619bd6faa84921c13c549
Amit <amitkvongc2004@gmail.com>
2024-06-28 16:34:20 +0530

    Faltu ka commit

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
      path: "/about", 
      element: <><Navbar/><About /></>,
     },
-    
+    //njbnhbsvhfvbh
     {
       path: "/signup", 
       element: <> <SignupFormDemo /></>,
